4d2ce54582ffc4e511a0888ef8d557d228fcdf7e
Haritha0705 <harithawikramasinha2003@gmail.com>
2026-01-01 15:51:23 +0530

    Refactor Docker setup and add Express backend with MongoDB integration

---
+++ multi-docker-compose/backend/index.js
@@ -0,0 +1,51 @@
+// javascript
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import User from "./User.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 2000;
+const MONGODB_URL = process.env.MONGO_URL;
+
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+};
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect(MONGODB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+app.post("/users", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+app.get("/", (req, res) => res.json({ message: "Server Working !" }));
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Backend running on port ${PORT}`);
+});
